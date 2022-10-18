8f78e1ed90f263b11387eb72d8330b9c779cb1f6
Oleg Sorokin <style-x55@mail.ru>
2022-10-18 16:31:21 +0600

    Add shebang and ImportCommand to CLI application

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,15 @@
+#!/usr/bin/env node
+
 import CLIApplication from './clie-command/cli-application.js';
 import HelpCommand from './clie-command/help-command.js';
 import VersionCommand from './clie-command/version-command.js';
+import ImportCommand from './clie-command/import-command.js';
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
   new HelpCommand(),
-  new VersionCommand()
+  new VersionCommand(),
+  new ImportCommand()
 ]);
 
 myManager.processCommand(process.argv);
