6199847750223180d4d28b7e853262b238cc746e
Oleg Sorokin <style-x55@mail.ru>
2022-10-31 22:15:53 +0600

    Update ConfigSchema

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -1,7 +1,12 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type ConfigSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -10,5 +15,17 @@ export const configSchema = convict<ConfigSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
   }
 });
