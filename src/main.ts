7e031f950f623db04482e55dad4e4b0ba0fe925d
Oleg Sorokin <style-x55@mail.ru>
2022-10-31 22:27:34 +0600

    Add container

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { Container } from 'inversify';
 import Application from './app/application.js';
 import LoggerService from './common/logger/logger.service.js';
 import ConfigService from './common/config/config.service';
+import { Component } from './types/component.types.js';
+import { ConfigInterface } from './common/config/config.interface.js';
+import { LoggerInterface } from './common/logger/logger.interface.js';
 
-const logger = new LoggerService();
-const config = new ConfigService(logger);
+const applicationContainer = new Container();
+applicationContainer.bind<Application>(Component.Application).to(Application);
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
 
-const application = new Application(logger, config);
+const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
