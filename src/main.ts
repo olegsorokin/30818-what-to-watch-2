5ddba96ab56dac8af57d5a991f368842ae33977d
Oleg Sorokin <style-x55@mail.ru>
2022-10-31 22:49:20 +0600

    Add "inSingletonScope"

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import { ConfigInterface } from './common/config/config.interface.js';
 import { LoggerInterface } from './common/logger/logger.interface.js';
 
 const applicationContainer = new Container();
-applicationContainer.bind<Application>(Component.Application).to(Application);
-applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
-applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
+applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
